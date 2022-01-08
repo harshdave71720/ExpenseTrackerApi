24a430cea74d8fe5ff8dc9315819c2b581a4ef46
Harsh Dave <harshdave71720@gmail.com>
2022-01-08 12:56:47 +0530

    Added And Operator and tests

--- ExpenseTracker.Core/Helpers/CustomFilters/Operators/AndOperator.cs
+++ ExpenseTracker.Core/Helpers/CustomFilters/Operators/AndOperator.cs
@@ -1,13 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Linq;
 
 namespace ExpenseTracker.Core.Helpers.CustomFilters
 {
-    public abstract class AndOperator : MultiOperator
+    public class AndOperator : MultiOperator
     {
         public AndOperator(List<Node> operands) : base(nameof(AndOperator), operands)
         {
+            _operands = operands;
+        }
+
+        public override Expression ToExpression()
+        {
+            return ToExpression(0);     
+        }
+        
+        private Expression ToExpression(int operandNumber)
+        {
+            if(operandNumber == _operands.Count() - 1)
+                return _operands[operandNumber].ToExpression();
+
+            return Expression.And(_operands[operandNumber].ToExpression(), ToExpression(operandNumber + 1));
         }
     }
 }
--- ExpenseTracker.Tests/Core/Helpers/CustomFilters/OperatorTests.cs
+++ ExpenseTracker.Tests/Core/Helpers/CustomFilters/OperatorTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using ExpenseTracker.Core.Helpers.CustomFilters;
 using Moq;
 using System.Linq.Expressions;
+using System.Collections.Generic;
 
 namespace ExpenseTracker.Tests.Core
 {
@@ -52,6 +53,60 @@ namespace ExpenseTracker.Tests.Core
             // Assert
             operand1.Verify(o => o.ToExpression(), Times.Once);
             operand2.Verify(o => o.ToExpression(), Times.Once);
+        }
+        #endregion
+
+        #region AndOperator
+        
+        [Test]
+        public void Cosntructor_SetsNameToAndOperator()
+        {
+            // Arrange
+            Operator andOperator = new AndOperator(new List<Node>());
+
+            // Assert
+            Assert.AreEqual(nameof(AndOperator), andOperator.Name);
+        }
+
+        [Test]
+        public void ToExpression_ReturnsAndOperator()
+        {
+            // Arrange
+            Operator andOperator = new AndOperator(new List<Node>());
+
+            // Act
+            Expression result = andOperator.ToExpression();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(ExpressionType.And, result.NodeType);
+        }
+
+
+        [Test]
+        public void ToExpression_()
+        {
+            // Arrange
+            Mock<Node> operand1 = new Mock<Node>();
+            Mock<Node> operand2 = new Mock<Node>();
+            operand1.Setup(o => o.ToExpression()).Returns(Expression.Constant(1));
+            operand2.Setup(o => o.ToExpression()).Returns(Expression.Constant(1));
+            Node exp1 = new EqualsOperator(operand1.Object, operand2.Object);
+
+            Mock<Node> operand3 = new Mock<Node>();
+            Mock<Node> operand4 = new Mock<Node>();
+            operand3.Setup(o => o.ToExpression()).Returns(Expression.Constant(1));
+            operand4.Setup(o => o.ToExpression()).Returns(Expression.Constant(1));
+            Node exp2 = new EqualsOperator(operand3.Object, operand4.Object);
+
+            Operator andOperator = new AndOperator(new List<Node> { exp1, exp2 });
+
+            // Act
+            Expression result = andOperator.ToExpression();
+
+            // Assert
+            Assert.IsNotNull(result);
+            // Assert.AreEqual("wrong", result.ToString());
         }
         #endregion
     }
