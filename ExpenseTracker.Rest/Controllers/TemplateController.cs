c5f0013205148711e077186c761d39b6d9e3d189
Harsh Dave <harshdave71720@gmail.com>
2022-01-17 21:20:59 +0530

    Added endpoint that returns expense template

---
+++ ExpenseTracker.Rest/Controllers/TemplateController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace ExpenseTracker.Rest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemplateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("expense")]
+        public IActionResult GetExpenseUploadTemplate()
+        {
+            Stream stream = new FileStream("./Templates/Expense_Upload_Template.csv", FileMode.Open, FileAccess.Read);
+            if(stream == null)
+                return NotFound();
+
+            return File(stream, "text/csv");
+        }
+    }
+}
