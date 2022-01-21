3c9e46e2493a290949d7ca51cf9923ec99d1f6d7
Harsh Dave <harshdave71720@gmail.com>
2022-01-21 11:37:06 +0530

    Added TemplateService.cs

---
+++ ExpenseTracker.Core/Services/ITemplateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Core.Services
+{
+    public interface ITemplateService
+    {
+        Task<IEnumerable<T>> GetRecordsFromTemplate<T>(Stream stream);
+    }
+}
---
+++ ExpenseTracker.Core/Services/TemplateService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTracker.Core.Helpers.Templates;
+
+namespace ExpenseTracker.Core.Services
+{
+    public class TemplateService : ITemplateService
+    {
+        public async Task<IEnumerable<T>> GetRecordsFromTemplate<T>(Stream stream) 
+        {
+            Template<T> template = new Template<T>(stream);
+            return await template.GetRecords();
+        }
+    }
+}
