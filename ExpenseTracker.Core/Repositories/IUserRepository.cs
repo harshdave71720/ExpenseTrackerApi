6a10c3a6f59f57cf16d61158cc9e0e3afd57a6a1
Harsh Dave <harshdave71720@gmail.com>
2022-02-07 19:11:48 +0530

    Added interface

---
+++ ExpenseTracker.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTracker.Core.Entities;
+
+namespace ExpenseTracker.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUser(string email);
+    }
+}
