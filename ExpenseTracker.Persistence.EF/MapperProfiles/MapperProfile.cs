2765f107f25211ac3e7acc617ceb2b3519c8241f
Harsh Dave <harshdave71720@gmail.com>
2022-02-07 21:00:14 +0530

    Upated Mapper profiles

--- ExpenseTracker.Persistence.EF/MapperProfiles/MapperProfile.cs
+++ ExpenseTracker.Persistence.EF/MapperProfiles/MapperProfile.cs
@@ -10,9 +10,13 @@ namespace ExpenseTracker.Persistence
         {
             CreateMap<Expense, ExpenseEntity>()
                 .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Category.Id))
-                .ForMember(dest => dest.Category, opt => opt.Ignore()).MaxDepth(1);
+                .ForMember(dest => dest.Category, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
+                .MaxDepth(1);
             CreateMap<ExpenseEntity, Expense>().MaxDepth(1);
-            CreateMap<Category, CategoryEntity>().MaxDepth(1);
+            CreateMap<Category, CategoryEntity>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.User.Id))
+                .MaxDepth(1);
             CreateMap<CategoryEntity, Category>().MaxDepth(1);
             CreateMap<UserEntity, User>();
         }
