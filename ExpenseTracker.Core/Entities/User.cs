04a516cf2d2e371588d9f0c2929d883b72ccb65a
Harsh Dave <harshdave71720@gmail.com>
2022-02-07 13:54:18 +0530

    Added User Business Entity

---
+++ ExpenseTracker.Core/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Core.Entities
+{
+    public class User
+    {
+        public readonly int Id;
+
+        public readonly string Email;
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public User(int id, string email, string firstName, string lastName)
+        {
+            Id = id;
+            Email = email;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
