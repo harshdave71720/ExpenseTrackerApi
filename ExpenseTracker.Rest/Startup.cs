deecb1f345c26976e65a57173e31596f1087e797
Harsh Dave <harshdave71720@gmail.com>
2022-02-14 18:32:36 +0530

    Added User Service to DI

--- ExpenseTracker.Rest/Startup.cs
+++ ExpenseTracker.Rest/Startup.cs
@@ -70,6 +70,7 @@ namespace ExpenseTracker.Rest
             services.AddTransient<IExpenseService, ExpenseService>();  
             services.AddTransient<ICategoryService, CategoryService>();
             services.AddTransient<ITemplateService, TemplateService>();
+            services.AddTransient<IUserService, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
