1541084022295ef6871a997202742b0db94b2743
Harsh Dave <harshdave71720@gmail.com>
2022-02-23 08:58:11 +0530

    Added Register and GetToken

--- ExpenseTracker.Rest/Controllers/UserController.cs
+++ ExpenseTracker.Rest/Controllers/UserController.cs
@@ -5,6 +5,10 @@ using ExpenseTracker.Core.Services;
 using AutoMapper;
 using ExpenseTracker.Rest.Dtos;
 using ExpenseTracker.Core.Entities;
+using ExpenseTracker.Identity.Dtos;
+using ExpenseTracker.Identity.Services;
+using System.Transactions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ExpenseTracker.Rest.Controllers
 {
@@ -13,16 +17,19 @@ namespace ExpenseTracker.Rest.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IIdentiyUserService _identityUserService;
         private readonly IMapper _mapper;
 
-        public UserController(IUserService userService, IMapper mapper)
+        public UserController(IUserService userService, IMapper mapper, IIdentiyUserService IdentityUserService)
         { 
             _userService = userService;
             _mapper = mapper;
+            _identityUserService = IdentityUserService;
         }
 
         [Route("")]
         [HttpGet]
+        [Authorize(policy: "LastNamePolicy")]
         public async Task<IActionResult> Get(string email)
         {
             var user = await this._userService.Get(email);
@@ -33,19 +40,44 @@ namespace ExpenseTracker.Rest.Controllers
             return Ok(_mapper.Map<UserDto>(user));
         }
 
-        [Route("")]
+        [Route("register")]
         [HttpPost]
-        public async Task<IActionResult> Add(UserDto user)
+        public async Task<IActionResult> Register(RegisterUserDto user)
         { 
             if(!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var addedUser = await _userService.Add(_mapper.Map<User>(user));
+            User addedUser = null;
+            using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await _identityUserService.Register(user);
+                addedUser = await _userService.Add(_mapper.Map<User>(user));
+
+                scope.Complete();
+            }
+
+            //await _identityUserService.Register(user);
+            //addedUser = await _userService.Add(_mapper.Map<User>(user));
 
             if (addedUser == null)
                 return BadRequest();
 
             return Ok(_mapper.Map<UserDto>(addedUser));
+        }
+
+        [Route("token")]
+        [HttpPost]
+        public async Task<IActionResult> GetToken([FromBody]LoginUserDto user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var token = await _identityUserService.GetToken(user);
+
+            if(token == null)
+                return Forbid();
+
+            return Ok(new { Token = token });
         }
     }
 }
