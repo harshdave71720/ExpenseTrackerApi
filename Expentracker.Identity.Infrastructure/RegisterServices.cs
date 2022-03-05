76a5f8b7ba616d4b978a2a3787d0142a1a2b5ebb
Harsh Dave <harshdave71720@gmail.com>
2022-03-05 12:31:11 +0530

    Added more jwt configs

--- Expentracker.Identity.Infrastructure/RegisterServices.cs
+++ Expentracker.Identity.Infrastructure/RegisterServices.cs
@@ -36,14 +36,15 @@ namespace ExpenseTracker.Identity.Infrastructure
                 .AddJwtBearer(options =>
                 {
                     options.TokenValidationParameters.ValidateIssuer = true;
-                    //options.TokenValidationParameters.ValidateAudience = true;
+                    options.TokenValidationParameters.ValidateAudience = true;
                     options.TokenValidationParameters.ValidIssuer = Configuration["JwtConfiguration:Issuer"];
-                    //options.TokenValidationParameters.ValidAudience = Configuration["JwtConfiguration:Audience"];
+                    options.TokenValidationParameters.ValidAudience = Configuration["JwtConfiguration:Audience"];
                     options.TokenValidationParameters.ValidateIssuerSigningKey = true;
                     options.TokenValidationParameters.ValidateLifetime = true;
                     options.SaveToken = false;
                     options.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(@Configuration["JwtConfiguration:Secret"]));
                 });
+
             services.AddScoped<UserManager<ApplicationUserDbEntity>>();
             services.AddScoped<IIdentiyUserService, IdentityUserService>();
             return services;
