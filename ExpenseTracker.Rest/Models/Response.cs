ec685dea023ac0f17cdbe43fb8cb4dfa3d5bd338
Harsh Dave <harshdave71720@gmail.com>
2022-03-08 09:39:28 +0530

    Added Response Class

---
+++ ExpenseTracker.Rest/Models/Response.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Rest.Models
+{
+    public class Response
+    {
+        [JsonProperty(Order = 1)]
+        public int StatusCode { get; set; }
+
+        [JsonProperty(Order = 2)]
+        public object Data { get; set; }
+
+        [JsonProperty(Order = 3)]
+        public IEnumerable<string> Errors { get; set; }
+
+        public Response(int statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public Response(int statusCode, object data) : this(statusCode)
+        { 
+            this.Data = data;
+        }
+
+        public Response(int statusCode, IEnumerable<string> errors) : this(statusCode)
+        { 
+            this.Errors = errors;
+        }
+    }
+}
